736f25a6d9ea68f6281fb4f216d7b1ffd85910f3
Ahmad Awais <me@AhmadAwais.com>
2018-03-24 00:05:31 +0500

    👌 IMPROVE: Standard formatting

--- wposa-init.php
+++ wposa-init.php
@@ -17,7 +17,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-
 /**
  * Define global constants.
  *
@@ -25,19 +24,19 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 // Plugin version.
 if ( ! defined( 'WPOSA_VERSION' ) ) {
-    define( 'WPOSA_VERSION', '1.0.0' );
+	define( 'WPOSA_VERSION', '1.0.0' );
 }
 
 if ( ! defined( 'WPOSA_NAME' ) ) {
-    define( 'WPOSA_NAME', trim( dirname( plugin_basename( __FILE__ ) ), '/' ) );
+	define( 'WPOSA_NAME', trim( dirname( plugin_basename( __FILE__ ) ), '/' ) );
 }
 
-if ( ! defined('WPOSA_DIR' ) ) {
-    define( 'WPOSA_DIR', WP_PLUGIN_DIR . '/' . WPOSA_NAME );
+if ( ! defined( 'WPOSA_DIR' ) ) {
+	define( 'WPOSA_DIR', WP_PLUGIN_DIR . '/' . WPOSA_NAME );
 }
 
-if ( ! defined('WPOSA_URL' ) ) {
-    define( 'WPOSA_URL', WP_PLUGIN_URL . '/' . WPOSA_NAME );
+if ( ! defined( 'WPOSA_URL' ) ) {
+	define( 'WPOSA_URL', WP_PLUGIN_URL . '/' . WPOSA_NAME );
 }
 
 /**
@@ -45,7 +44,7 @@ if ( ! defined('WPOSA_URL' ) ) {
  *
  * Initializes the WP-OOP-Settings-API.
  *
- * @since 	1.0.0
+ * @since   1.0.0
  */
 
 
@@ -54,9 +53,7 @@ if ( ! defined('WPOSA_URL' ) ) {
  *
  * @since 1.0.0
  */
-if ( file_exists( WPOSA_DIR . '/class-wposa.php' ) ) {
-    require_once( WPOSA_DIR . '/class-wposa.php' );
-}
+require_once WPOSA_DIR . '/class-wp-osa.php';
 
 
 /**
@@ -75,23 +72,21 @@ if ( class_exists( 'WP_OSA' ) ) {
 	$wposa_obj = new WP_OSA();
 
 
-    // Section: Basic Settings.
-    $wposa_obj->add_section(
-    	array(
+	// Section: Basic Settings.
+	$wposa_obj->add_section(
+		array(
 			'id'    => 'wposa_basic',
 			'title' => __( 'Basic Settings', 'WPOSA' ),
 		)
-    );
+	);
 
-    // Section: Other Settings.
-    $wposa_obj->add_section(
-    	array(
+	// Section: Other Settings.
+	$wposa_obj->add_section(
+		array(
 			'id'    => 'wposa_other',
 			'title' => __( 'Other Settings', 'WPOSA' ),
 		)
-    );
-
-
+	);
 
 	// Field: Text.
 	$wposa_obj->add_field(
@@ -105,8 +100,6 @@ if ( class_exists( 'WP_OSA' ) ) {
 		)
 	);
 
-
-
 	// Field: Number.
 	$wposa_obj->add_field(
 		'wposa_basic',
@@ -116,24 +109,20 @@ if ( class_exists( 'WP_OSA' ) ) {
 			'name'              => __( 'Number Input', 'WPOSA' ),
 			'desc'              => __( 'Number field with validation callback `intval`', 'WPOSA' ),
 			'default'           => 1,
-			'sanitize_callback' => 'intval'
+			'sanitize_callback' => 'intval',
 		)
 	);
-
-
 
 	// Field: Password.
 	$wposa_obj->add_field(
 		'wposa_basic',
 		array(
-			'id'      => 'password',
-			'type'    => 'password',
-			'name'    => __( 'Password Input', 'WPOSA' ),
-			'desc'    => __( 'Password field description', 'WPOSA' ),
+			'id'   => 'password',
+			'type' => 'password',
+			'name' => __( 'Password Input', 'WPOSA' ),
+			'desc' => __( 'Password field description', 'WPOSA' ),
 		)
 	);
-
-
 
 	// Field: Textarea.
 	$wposa_obj->add_field(
@@ -146,30 +135,24 @@ if ( class_exists( 'WP_OSA' ) ) {
 		)
 	);
 
-
-
 	// Field: Separator.
 	$wposa_obj->add_field(
 		'wposa_basic',
 		array(
-			'id'      => 'separator',
-			'type'    => 'separator',
+			'id'   => 'separator',
+			'type' => 'separator',
 		)
 	);
-
-
 
 	// Field: Title.
 	$wposa_obj->add_field(
 		'wposa_basic',
 		array(
-			'id'      => 'title',
-			'type'    => 'title',
-			'name'    => '<h1>Title</h1>',
+			'id'   => 'title',
+			'type' => 'title',
+			'name' => '<h1>Title</h1>',
 		)
 	);
-
-
 
 	// Field: Checkbox.
 	$wposa_obj->add_field(
@@ -182,90 +165,78 @@ if ( class_exists( 'WP_OSA' ) ) {
 		)
 	);
 
-
-
 	// Field: Radio.
 	$wposa_obj->add_field(
 		'wposa_basic',
 		array(
-			'id'   => 'radio',
-			'type' => 'radio',
-			'name' => __( 'Radio', 'WPOSA' ),
-			'desc' => __( 'Radio Button', 'WPOSA' ),
+			'id'      => 'radio',
+			'type'    => 'radio',
+			'name'    => __( 'Radio', 'WPOSA' ),
+			'desc'    => __( 'Radio Button', 'WPOSA' ),
 			'options' => array(
 				'yes' => 'Yes',
-				'no'  => 'No'
-			)
+				'no'  => 'No',
+			),
 		)
 	);
-
-
 
 	// Field: Multicheck.
 	$wposa_obj->add_field(
 		'wposa_basic',
 		array(
-			'id'   => 'multicheck',
-			'type' => 'multicheck',
-			'name' => __( 'Multile checkbox', 'WPOSA' ),
-			'desc' => __( 'Multile checkbox description', 'WPOSA' ),
+			'id'      => 'multicheck',
+			'type'    => 'multicheck',
+			'name'    => __( 'Multile checkbox', 'WPOSA' ),
+			'desc'    => __( 'Multile checkbox description', 'WPOSA' ),
 			'options' => array(
 				'yes' => 'Yes',
-				'no'  => 'No'
-			)
+				'no'  => 'No',
+			),
 		)
 	);
-
-
 
 	// Field: Select.
 	$wposa_obj->add_field(
 		'wposa_basic',
 		array(
-			'id'   => 'select',
-			'type' => 'select',
-			'name' => __( 'A Dropdown', 'WPOSA' ),
-			'desc' => __( 'A Dropdown description', 'WPOSA' ),
+			'id'      => 'select',
+			'type'    => 'select',
+			'name'    => __( 'A Dropdown', 'WPOSA' ),
+			'desc'    => __( 'A Dropdown description', 'WPOSA' ),
 			'options' => array(
 				'yes' => 'Yes',
-				'no'  => 'No'
-			)
+				'no'  => 'No',
+			),
 		)
 	);
-
-
 
 	// Field: Image.
 	$wposa_obj->add_field(
 		'wposa_other',
 		array(
-			'id'   => 'image',
-			'type' => 'image',
-			'name' => __( 'Image', 'WPOSA' ),
-			'desc' => __( 'Image description', 'WPOSA' ),
+			'id'      => 'image',
+			'type'    => 'image',
+			'name'    => __( 'Image', 'WPOSA' ),
+			'desc'    => __( 'Image description', 'WPOSA' ),
 			'options' => array(
-				'button_label' => 'Choose Image'
-			)
+				'button_label' => 'Choose Image',
+			),
 		)
 	);
-
-
 
 	// Field: File.
 	$wposa_obj->add_field(
 		'wposa_other',
 		array(
-			'id'   => 'file',
-			'type' => 'file',
-			'name' => __( 'File', 'WPOSA' ),
-			'desc' => __( 'File description', 'WPOSA' ),
+			'id'      => 'file',
+			'type'    => 'file',
+			'name'    => __( 'File', 'WPOSA' ),
+			'desc'    => __( 'File description', 'WPOSA' ),
 			'options' => array(
-				'button_label' => 'Choose file'
-			)
+				'button_label' => 'Choose file',
+			),
 		)
 	);
-
-
 
 	// Field: Color.
 	$wposa_obj->add_field(
@@ -278,8 +249,6 @@ if ( class_exists( 'WP_OSA' ) ) {
 		)
 	);
 
-
-
 	// Field: WYSIWYG.
 	$wposa_obj->add_field(
 		'wposa_other',
@@ -290,7 +259,4 @@ if ( class_exists( 'WP_OSA' ) ) {
 			'desc' => __( 'WP_Editor description', 'WPOSA' ),
 		)
 	);
-
-
-
 }
